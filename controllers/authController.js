26bc320e31d94601d9e0a791cf2391296b4b1413
Douglas <mercurymuur86@yahoo.com>
2023-03-09 00:25:45 -0500

    jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,18 @@
 const User = require('../models/User');
+const jwt = require ('jsonwebtoken');
 
 const handleErrors = (err) => {
     console.log(err.message, err.code);
     let errors = {email: '', password: ''};
+
+    //incorrect email
+    if(err.message === 'incorrect email'){
+        errors.email = 'That email is not registered';
+    }
+
+    if(err.message === 'incorrect password'){
+        errors.password = 'That password is not correct';
+    }
 
     //duplicate errors
     if(err.code === 11000){
@@ -18,6 +28,13 @@ const handleErrors = (err) => {
     return errors;
 }
 
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+    return jwt.sign({ id}, 'secret message', {
+        expiresIn: maxAge
+    });
+}
+
 //controller actions
 module.exports.signup_get = (req, res) => {
     res.render('signup');
@@ -30,10 +47,12 @@ module.exports.login_get = (req, res) => {
 module.exports.signup_post = async (req, res) => {
     const {email, password} = req.body;
     try{
-       const user = await User.create({email, password}); res.status(201).json(user);
+       const user = await User.create({email, password});
+       const token = createToken(user._id);
+       res.cookie ('jwt', token, {httpOnly: true, maxAge: maxAge * 1000});
+        res.status(201).json({ user: user._id });
     } catch(err){
         const errors = handleErrors(err);
-        handleErrors(err);
         res.status(400).send({ errors })
     }
 };
@@ -42,6 +61,15 @@ module.exports.login_post =  async (req, res) => {
     const {email, password} = req.body;
 
 
-    console.log(email, password);
-    res.send('user login');
+    //console.log(email, password);
+    //res.send('user login');
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000});
+        res.status(200).json({ user: user._id});
+    } catch(err){
+        const errors = handleErrors(err)
+        res.status(400).json({ errors });
+    }
 };
--- models/User.js
+++ models/User.js
@@ -7,7 +7,8 @@ const userSchema = new mongoose.Schema ({
         type: String,
         required: [true, 'Please enter an email'],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, 'please enter a valid email']
     },
     password: {
         type: String,
@@ -28,6 +29,18 @@ userSchema.pre('save', async function(next) {
     this.password = await bcrypt.hash(this.password, salt);
     next();
 });
+
+userSchema.statics.login = async function(email, password){
+    const user = await this.findOne({ email });
+    if(user){
+        const auth = await bcrypt.compare(password, user.password);
+        if(auth){
+            return user;
+        }
+        throw Error('incorrect password');
+    }
+    throw Error ('incorrect email');
+}
 
 const User = mongoose.model('user', userSchema)
 
